1bf1cc594c4c3494285e1de61b5d293fa3688dbb
Evan Eliason <evaneliason1@gmail.com>
2024-06-05 17:58:46 -0700

    likes and error, 81/81 passed in postman

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,46 +1,96 @@
 const Item = require("../models/clothingItem.js");
+const {
+  BAD_INPUT_ERROR_CODE,
+  NO_RES_ERROR_CODE,
+  DEFAULT_ERROR_CODE,
+} = require("../utils/errors.js");
 
 const getItems = (req, res) => {
   Item.find({})
     .then((items) => res.status(200).send(items))
     .catch((err) => {
       console.error(err);
-      return res.status(500).send({ message: err.message });
+      return res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
     });
 };
 
 const createItem = (req, res) => {
   console.log(req.user._id);
-  // const owner = req.user._id;
 
   const { name, weather, imageUrl, owner } = req.body;
 
   Item.create({ name, weather, imageUrl, owner: req.user._id })
     .then((item) => {
-      res.status(201).send({data: item});
+      res.status(201).send({ data: item });
     })
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
-        return res.status(400).send({ message: err.message });
+        return res.status(BAD_INPUT_ERROR_CODE).send({ message: err.message });
       }
-      return res.status(500).send({ message: err.message });
+      return res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
     });
 };
 
 const deleteItem = (req, res) => {
-  // const { name, avatar } = req.body;
-  // User.create({ name, avatar })
-  //   .then((user) => {
-  //     res.status(201).send(user);
-  //   })
-  //   .catch((err) => {
-  //     console.error(err);
-  //     if (err.name === "ValidationError") {
-  //       return res.status(400).send({ message: err.message });
-  //     }
-  //     return res.status(500).send({ message: err.message });
-  //   });
+  const { itemId } = req.params;
+
+  Item.findByIdAndRemove(itemId)
+    .orFail()
+    .then((item) => {
+      res.status(200).send({ data: item });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res.status(BAD_INPUT_ERROR_CODE).send({ message: err.message });
+      } else if (err.name === "DocumentNotFoundError") {
+        return res.status(NO_RES_ERROR_CODE).send({ message: err.message });
+      }
+      return res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
+    });
 };
 
-module.exports = { getItems, createItem, deleteItem };
+const likeItem = (req, res) => {
+  Item.findByIdAndUpdate(
+    req.params.itemId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail()
+    .then((item) => {
+      res.status(200).send({ data: item });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res.status(BAD_INPUT_ERROR_CODE).send({ message: err.message });
+      } else if (err.name === "DocumentNotFoundError") {
+        return res.status(NO_RES_ERROR_CODE).send({ message: err.message });
+      }
+      return res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
+    });
+};
+
+const dislikeItem = (req, res) => {
+  Item.findByIdAndUpdate(
+    req.params.itemId,
+    { $pull: { likes: req.user._id } },
+    { new: true }
+  )
+    .orFail()
+    .then((item) => {
+      res.status(200).send({ data: item });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.name === "CastError") {
+        return res.status(BAD_INPUT_ERROR_CODE).send({ message: err.message });
+      } else if (err.name === "DocumentNotFoundError") {
+        return res.status(NO_RES_ERROR_CODE).send({ message: err.message });
+      }
+      return res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
+    });
+};
+
+module.exports = { getItems, createItem, deleteItem, likeItem, dislikeItem };
--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,21 @@
 const User = require("../models/user.js");
+const {
+  BAD_INPUT_ERROR_CODE,
+  NO_RES_ERROR_CODE,
+  DEFAULT_ERROR_CODE,
+} = require("../utils/errors.js");
 
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
     .catch((err) => {
       console.error(err);
-      return res.status(500).send({ message: err.message });
+      if (err.name === "ValidationError") {
+        return res.status(BAD_INPUT_ERROR_CODE).send({ message: err.message });
+      } else if (err.name === "DocumentNotFoundError") {
+        return res.status(NO_RES_ERROR_CODE).send({ message: err.message });
+      }
+      return res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
     });
 };
 
@@ -13,16 +23,17 @@ const getUser = (req, res) => {
   const { userId } = req.params;
 
   User.findById(userId)
+    .orFail()
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       console.error(err);
       console.log(err.name);
       if (err.name === "CastError") {
-        return res.status(400).send({ message: err.message });
+        return res.status(BAD_INPUT_ERROR_CODE).send({ message: err.message });
       } else if (err.name === "DocumentNotFoundError") {
-        return res.status(404).send({ message: err.message });
+        return res.status(NO_RES_ERROR_CODE).send({ message: err.message });
       }
-      return res.status(500).send({ message: err.message });
+      return res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
     });
 };
 
@@ -35,9 +46,9 @@ const createUser = (req, res) => {
     .catch((err) => {
       console.error(err);
       if (err.name === "ValidationError") {
-        return res.status(400).send({ message: err.message });
+        return res.status(BAD_INPUT_ERROR_CODE).send({ message: err.message });
       }
-      return res.status(500).send({ message: err.message });
+      return res.status(DEFAULT_ERROR_CODE).send({ message: err.message });
     });
 };
 
